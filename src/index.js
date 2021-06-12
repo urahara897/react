738def8e3246827db348f95605718c33430fd2d6
urahara897 <84304202+urahara897@users.noreply.github.com>
2021-06-12 20:04:39 +0530

    adding to github

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import Counters from './Components/counters';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counters />
   </React.StrictMode>,
   document.getElementById('root')
 );
